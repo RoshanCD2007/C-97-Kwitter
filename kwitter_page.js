c18ba100ed457f949d35a4b4b19ab7c5cfee9a66
RoshanCD2007 <81854107+RoshanCD2007@users.noreply.github.com>
2021-07-07 17:46:15 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -26,7 +26,7 @@ message=message_data['message'];
 like=message_data['like'];
 name_with_tag="<h4>"+name+"<img src='tick.png' class='user_tick'></h4>";
 message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
-like_button="<button class='btn btn-warning' id="+firebase_message_id+"value:"+like+" onclick= 'updateLike(this.id)'>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick= 'updateLike(this.id)'>";
 span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like:"+like+"</span></button><hr>";
 row=name_with_tag+message_with_tag+like_button+span_with_tag;
 document.getElementById("output").innerHTML+=row;
